f970dcf39aec2cc507d34a33443264d6c417d675
OTDavid7 <92827464+OTDavid7@users.noreply.github.com>
2022-05-02 09:03:58 -0700

    dd

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var fullName = "what is your name"
+
+function checkout () {
+    alert("See my contact on the web and choose the most convienent method for you to get started. Thank you");
+    prompt(fullName);
+}
